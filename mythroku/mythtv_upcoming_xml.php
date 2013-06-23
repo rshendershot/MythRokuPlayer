fb8630ba3e6b5cf9337eeda8e80753215556c0c0
Richard <rshendershot@gmail.com>
2013-06-23 10:02:43 -0500

    oops

--- mythroku/mythtv_upcoming_xml.php
+++ mythroku/mythtv_upcoming_xml.php
@@ -76,7 +76,7 @@ if(isset($_GET['Upcoming'])) {
 	print $feed;
 	
 }else{
-	// build category static groupings: All $limit, All	
+	// build category static groupings: Top $limit, All	
 
 	$upcoming = new category(
 		array(XmlEmitter::ATR.'title'=>'Upcoming'
